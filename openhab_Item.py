99ba078b3ecb965c585baaa3fee7348bc4f75619
Michdo93 <michaeldoerflinger93@gmail.com>
2022-09-20 21:42:51 +0200

    Update openhab_Item.py

--- openhab_Item.py
+++ openhab_Item.py
@@ -28,6 +28,46 @@ class Item(object):
         
     def setGroups(self, groups:list):
         self.groups = groups
+
+class ColorItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class ContactItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class DateTimeItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class DimmerItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class GroupItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class ImageItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class LocationItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class NumberItem(Item):
+    def __init__(self, type:str = None, name:str = None, state = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class PlayerItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
+        
+class RollershutterItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
         
 class StringItem(Item):
     def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
@@ -39,3 +79,7 @@ class StringItem(Item):
     
     def setState(self, state:str):
         self.state = state
+
+class SwitchItem(Item):
+    def __init__(self, type:str = None, name:str = None, state:str = None, tags:list = None, groups:list = None):
+        super().__init__(type, name, tags, groups)
